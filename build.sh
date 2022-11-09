8e4df2d24a7d183b2c11e6b762ef57f8a8e51c6e
gustavo domingues <deathdiec@gmail.com>
2022-11-09 08:53:09 -0300

    fix: .sh

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -m32 src/main.c src/spawn.c src/sound.c src/splashScreen.c src/input.c src/init.c src/draw.c src/hero.c -DSPLASH=0 -DFULLSCREEN=0 SDL2.dll SDL2_mixer.dll SDL2_image.dll -o heroes
