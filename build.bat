94fd02230e4b5657b30ebea4098e45535eb90298
gustavo domingues <deathdiec@gmail.com>
2022-11-18 11:03:21 -0300

    fix: No cmd

--- build.bat
+++ build.bat
@@ -1,1 +1,1 @@
-gcc src/main.c src/spawn.c src/sound.c src/splashScreen.c src/input.c src/init.c src/ball.c src/draw.c src/hero.c -DSPLASH=0 -DFULLSCREEN=0 SDL2.dll SDL2_mixer.dll SDL2_image.dll -o dist/heroes
+gcc src/main.c src/spawn.c src/sound.c src/splashScreen.c src/input.c src/init.c src/ball.c src/draw.c src/hero.c -mwindows -DSPLASH=0 -DFULLSCREEN=0 SDL2.dll SDL2_mixer.dll SDL2_image.dll -o dist/heroes
